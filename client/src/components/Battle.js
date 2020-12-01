ae9c9d21e5c8e7ff0fd41cbf4130c887ee27dcf9
violettaval <violetvalencia37@gmail.com>
2020-12-01 14:19:41 -0600

    fixed battle turn system to have a full order of combat

--- client/src/components/Battle.js
+++ client/src/components/Battle.js
@@ -1,5 +1,6 @@
 import React, { useEffect , useState } from "react";
 import { Button, Col, Row } from "react-bootstrap";
+//import character 
 
 // import { Container } from "react-bootstrap/lib/Tab";
 
@@ -7,32 +8,24 @@ function Battle() {
     //activates button and sets turn over should be false while dialogue is displaying
     const [turnbase, setTurnbase] = useState(false);
     const [screentext, setScreentext] =useState("");    
+    // const [player, setTurnbase] = useState();
+    // const [playerState, setDeveloperState] = useState({
+    //     maxHealth: 1000,
+    //     hitpoints: true,
+    //     age: true,
+    //     lifeLongLearner: true,
+    //     name: "Riley",
+    //   });
+    //   const [foeState, setDeveloperState] = useState({
+    //     maxHealth: 1000,
+    //     hitpoints: true,
+    //     age: true,
+    //     lifeLongLearner: true,
+    //     name: "Riley",
+    //   });
 
-    useEffect(() => {
-        speedRead(screentext, "A Slime appears to block your path Click on the options to initiate combat")
-        .then(() => {
-            setScreentext(screentext, "");
-            setTurnbase(true);
-        });
-    }, []);
-
-    async function speedRead(screentext,params) {
-        let i =0;
-        setScreentext(screentext, "");
-        let words = params.split(" ");
-        let textInterval = setInterval(function() {
-            if (words[i] === undefined) {
-            clearInterval(textInterval);
-            } 
-            if (i === words.length) {
-                return Promise;
-            }
-            else {
-                setScreentext(screentext += " " + words[i]);
-                i++;
-            }
-        }, 300);
-    }
+    const slime = new Character("Nice Slime", 1, 10, 50);
+    const rogue = new Character("Dodger", 1, 20, 100);
 
     function Character(name, age, strength, hitpoints) {
         this.name = name;
@@ -40,21 +33,54 @@ function Battle() {
         this.strength = strength;
         this.hitpoints = hitpoints;
         // //hitpoints is the value that changes out of total health
-        // this.health = hitpoints;
+        this.maxHealth = hitpoints;
     }
+
+    useEffect(() => {
+        speedRead(screentext, `A ${slime.name} appears to block your path Click on the options to initiate combat. `)
+        // .then(() => {
+            // setScreentext(screentext, "");
+        //     setTurnbase(turnbase,true);
+        //  console.log(turnbase);
+        // });
+    }, []);
+
+    async function speedRead(screentext,params) {
+        let i =0;
+        setScreentext("");
+        let words = params.split("");
+        let textInterval = setInterval(await function() {
+            if (words[i] === undefined) {
+            clearInterval(textInterval);
+            } 
+            if (i === words.length) {
+                console.log("done with dialogue");
+                // setScreentext("");
+                setTurnbase(true);
+                return Promise;
+            }
+            else {
+                setScreentext(screentext += words[i]);
+                i++;
+            }
+        }, 80);
+    }
+
+
     
         // method which takes in a second object and decreases their "hitpoints" by this character's strength
-    Character.prototype.attack = async function(character2) {
-        let dialogue = this.name + " readies an attack at " + character2.name + " " + this.name + " does " + this.strength + " damage to " + character2.name
+    Character.prototype.attack =  async function(character2) {
+        console.log(`${this.name} attacks ${character2}`)
+        let dialogue = (this.name + " readies an attack at " + character2.name + " " + this.name + " does " + this.strength + " damage to " + character2.name + ". ");
+        console.log(character2.hitpoints);
+        //
+
+
         character2.hitpoints -= this.strength;
-        speedRead(screentext, dialogue).then(
-            setScreentext(screentext, ""));
-        
-        if (character2.isAlive() === true && this.isAlive() === true){
-            //continue the fight
-            setTurnbase(true);
-        }
-        else{this.levelUp()};
+        await speedRead(screentext, dialogue);
+        console.log(`${character2.hitpoints} after damage`);
+        return ;
+        // setScreentext(screentext, "")
     };
       
     // method which increases this character's stats when called
@@ -67,7 +93,7 @@ function Battle() {
             this.name + " is now level " + this.age + ". Your Strength is now " + this.strength + 
             ". Your Health is now " +  this.hitpoints;
         speedRead(screentext, dialogue).then(() => {
-            setScreentext(screentext, "");
+            // setScreentext(screentext, "");
             window.location.href="localhost:3000/overworld";
         });
     };
@@ -77,37 +103,59 @@ function Battle() {
     Character.prototype.isAlive = function() {
         if (this.hitpoints > 0) {
             console.log(this.name + " is still alive!");
-            
             return true;
         }
         console.log(this.name + " has died!");
         return false;
     };
     
-    const slime = new Character("Nice Slime", 1, 10, 50);
-    const rogue = new Character("Dodger", 1, 20, 100);
-    
+    async function combat() {
+        console.log("combat starts");
+        try {
+            await rogue.attack(slime).then(() =>{
+
+                if (slime.isAlive() === false && rogue.isAlive() === true){
+                    //continue the fight
+                    rogue.levelUp();
+                }
+                if (rogue.isAlive() === false) {
+                    speedRead(screentext, "You have been defeated GAME OVER").then(() => {
+                        // setScreentext(screentext, "");
+                        window.location.href="localhost:3000/signin";
+                    });
+                }
+                else{
+                    slime.attack(rogue);
+                };
+            });
+            console.log(`Your hitpoints ${rogue.hitpoints}`);
+            setTurnbase(true);
+        }
+        catch(error) {
+            console.log(error);
+        };
+    }
     function handleBtnClick(event) {
         // Get the title of the clicked button
+        
         const btnName = event.target.getAttribute("data-value");
-        if (btnName === "Fight") {
-            if (turnbase === true){
+        if (turnbase === true){
+            //turn order is run
+            if (btnName === "Fight") {
+                setScreentext("");
                 setTurnbase(false);
-                //turn order is run
-                rogue.attack(slime);
-                slime.attack(rogue);
-                console.log(`Your hitpoints ${rogue.hitpoints}`);
-            }
+                combat();
+            } 
             else {
-                console.log("wait your turn");
+                console.log("You would like to run away");
+                if (Math.floor(Math.random()*2)>0){
+                    console.log("You ran away successfully");
+                    window.location.href="/overworld";
+                };
             };
-        } 
+        }
         else {
-            console.log("You would like to run away");
-            if (Math.floor(Math.random()*2)>0){
-                console.log("You ran away successfully");
-                window.location.href="/overworld";
-            };
+            console.log("wait your turn");
         };
     };
 
@@ -120,7 +168,7 @@ function Battle() {
             <Col>
                 <Button variant="primary" size="lg" data-value="Fight" onClick={handleBtnClick} >Fight</Button>
             </Col>
-            <Col></Col>
+            <Col><br/></Col>
             <Col>
                 <Button variant="secondary" size="lg" data-value="Run" onClick={handleBtnClick} >Run</Button>
             </Col>
