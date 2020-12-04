708e2b26a0fa10bd4b5c6b0ac9e7a1751e5909d2
Amanda Anderson <aanderson_120@yahoo.com>
2020-12-04 13:34:20 -0600

    merging

--- client/src/pages/Overworld/index.js
+++ client/src/pages/Overworld/index.js
@@ -67,7 +67,7 @@ const Overworld = () => {
           <div className="card overInst">
             {/* <div className="overworld"> */}
             {/* <div className="md:pl-4"> */}
-            <h3 style= {{ fontSize: 20 }}  className="italic">Use the arrow keys to run toward the enemy or away if it is too scary. Hint - if you run away you aren't fast enough so it's really best to face your fears. If your timing is right you can use the jump button to jump higher than your enemy, because they can't jump. They are filled with so much rage they can barely see straight, so jumping is hard for them. If your low on health you can jump towards a health item as it passess by. As a coder few things will keep you moving, so hopefully you get a good one.</h3>
+            <h3 style= {{ fontSize: 20 }}  className="italic">Use the arrow keys  to run toward the enemy or away if it is too scary. Hint - if you run away you aren't fast enough so it's really best to face your fears. If your timing is right you can use the jump button to jump higher than your enemy, because they can't jump. They are filled with so much rage they can barely see straight, so jumping is hard for them. If your low on health you can jump towards a health item as it passess by. As a coder few things will keep you moving, so hopefully you get a good one.</h3>
             {/* </div> */}
             <button className="signOut w-full py-3 bg-red-600 mt-4 text-white"
               onClick={() => {
